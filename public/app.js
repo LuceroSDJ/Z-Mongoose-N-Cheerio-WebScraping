9366556081c98e01bfb9d6733b8ad55014d98bd8
Carmen Lucero Sandoval <carmenlucero786@gmail.com>
2019-04-28 23:59:03 -0500

    grabed articles as json & displayed them on the page within a div dynamically created

--- public/app.js
+++ public/app.js
@@ -1,2 +1,17 @@
 // Get references to page elements
 var scrapeWeb = $("scrapeButton");
+var articlesDiv = $("#articlesDiv");
+
+// Grab the articles as a json
+$.getJSON("/articles", function(data) {
+    // For each one
+    for (var i = 0; i < data.length; i++) {
+        // Display the information on the page
+        // articlesDiv.append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+        articlesDiv.append("<div data-id='" + data[i]._id + "'>" + 
+        "<h1>" + data[i].title + "</h1>" + 
+        "<a href=" + data[i].link + ">" + data[i].link + "</a>" + "<br />" +
+        "<p>" + data[i].published + "</p>" +
+        "</div>");
+    }
+});
